91c826cd672a7420e390e5cbc3c22428882b42c9
Simon Aertssen <Simonaertssen@gmail.com>
2020-11-12 15:18:13 +0100

    looking to start soon

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -3,62 +3,61 @@
 using namespace std;
 
 // Investigate which computing approaches compute the fibonacci number the fastest.
-unsigned int fibonacci_iteration(unsigned int n);
-unsigned int fibonacci_recursion(unsigned int n);
+unsigned long long fibonacci_iteration(unsigned int n);
+unsigned long long fibonacci_recursion(unsigned int n);
 
 int main(int argc, char *argv[])
 {
     cout << "Testing fibonacci algorithms:" << endl;
-    int i, n = 15;
+    unsigned int n = 15;
     unsigned long long result;
-    result = factorial_iteration(n);
+    result = fibonacci_iteration(n);
     cout << "Iteration:  " << n << "! = " << result << endl;
 
-    result = factorial_recursion(n);
+    result = fibonacci_recursion(n);
     cout << "Recursion:  " << n << "! = " << result << endl;
 
-    result = factorial_accumulate(n);
-    cout << "Accumulate: " << n << "! = " << result << endl;
-
-    int *arr = new int[MAXNUM];
-    arr[0] = n;
-    factorial_precision(arr);
-    cout << "Precision:  " << n << "! = ";
-    for (i = arr[MAXNUM-1]-1; i >= 0; i--) cout << *(arr + i);
-    cout << endl << endl;
-
-    cout << "This method is also precise to up to " << MAXNUM << " digits:" << endl;
-    arr[0] = 50;
-    factorial_precision(arr);
-    cout << "Precision:  " << 50 << "! = ";
-    for (i = arr[MAXNUM-1]-1; i >= 0; i--) cout << *(arr + i);
-    cout << endl << endl;
-
-    unsigned int maxevals = 1.0e6;
-    cout << "The results are correct, let's time the performance for " << maxevals << " evaluations:" << endl;
-    cout << "Iteration:  "; timeme(factorial_iteration, n, maxevals);
-    cout << "Recursion:  "; timeme(factorial_recursion, n, maxevals);
-    cout << "Accumulate: "; timeme(factorial_accumulate, n, maxevals);
-    cout << "Precision:  "; timeme(factorial_precision, arr, maxevals);
+    // result = factorial_accumulate(n);
+    // cout << "Accumulate: " << n << "! = " << result << endl;
+    //
+    // int *arr = new int[MAXNUM];
+    // arr[0] = n;
+    // factorial_precision(arr);
+    // cout << "Precision:  " << n << "! = ";
+    // for (i = arr[MAXNUM-1]-1; i >= 0; i--) cout << *(arr + i);
+    // cout << endl << endl;
+    //
+    // cout << "This method is also precise to up to " << MAXNUM << " digits:" << endl;
+    // arr[0] = 50;
+    // factorial_precision(arr);
+    // cout << "Precision:  " << 50 << "! = ";
+    // for (i = arr[MAXNUM-1]-1; i >= 0; i--) cout << *(arr + i);
+    // cout << endl << endl;
+    //
+    // unsigned int maxevals = 1.0e6;
+    // cout << "The results are correct, let's time the performance for " << maxevals << " evaluations:" << endl;
+    // cout << "Iteration:  "; timeme(factorial_iteration, n, maxevals);
+    // cout << "Recursion:  "; timeme(factorial_recursion, n, maxevals);
+    // cout << "Accumulate: "; timeme(factorial_accumulate, n, maxevals);
+    // cout << "Precision:  "; timeme(factorial_precision, arr, maxevals);
 
     return 0;
 }
 
 unsigned long long fibonacci_iteration(unsigned int n){
-  unsigned long result = 1;
-  if (n < 2) return result;
+  if (n == 0) return 0;
+  if (n == 1) return 1;
+  unsigned long long x = 1, y = 1;
+  unsigned int i;
+  for (i = 0; i < n; i++){
 
-  int i;
-  for (i = 2; i < n+1; i++){
-    result *= i;
   }
-  return result;
+  return x;
 }
 
 
 unsigned long long fibonacci_recursion(unsigned int n){
-  if (n == 0){
-     return 1;
-  }
-  return n*factorial_recursion(n-1);
+  if (n == 0) return 0;
+  if (n == 1) return 1;
+  return factorial_recursion(n-1) + fibonacci_recursion(n-2);
 }
