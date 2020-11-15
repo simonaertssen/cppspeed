0932876fc19480ddc64958b53a97f1b25568551b
Simon Aertssen <Simonaertssen@gmail.com>
2020-11-15 16:58:58 +0100

    back to ints

--- factorials.cpp
+++ factorials.cpp
@@ -10,17 +10,17 @@ using namespace std;
 // to about log(2^53) ~ 15.955 decimal digits.
 // However, we can use some
 
-double factorial_iteration(short n);
-double *factorial_iteration_p(short *n);
-double factorial_recursion(short n);
-double accumulator(short n, double acc);
-double factorial_accumulate(short n);
+double factorial_iteration(int n);
+double *factorial_iteration_p(int *n);
+double factorial_recursion(int n);
+double accumulator(int n, double acc);
+double factorial_accumulate(int n);
 
-void factorial_precision(short *result);
+void factorial_precision(int *result);
 
 int main(int argc, char *argv[]){
     cout.precision(0);
-    short n = 20;
+    int n = 22;
 
     cout << "Testing factorial algorithms using naive multiplication:" << endl;
     double result = factorial_iteration(n);
@@ -37,7 +37,7 @@ int main(int argc, char *argv[]){
     cout << endl << endl;
 
     cout << "Testing factorial algorithms using arbitrary precision:" << endl;
-    short *arr = new short[MAXNUM];
+    int *arr = new int[MAXNUM];
     arr[0] = n;
     factorial_precision(arr);
     cout << "Precision:  " << n << "! = ";
@@ -45,7 +45,7 @@ int main(int argc, char *argv[]){
     for (i = arr[MAXNUM-1]-1; i >= 0; i--) cout << *(arr + i);
     cout << endl << endl;
 
-    unsigned int maxevals = 1.0e6;
+    unsigned int maxevals = 1.0e3;
     cout.precision(5);
     cout << "Let's time the performance for " << maxevals << " evaluations:" << endl;
     cout << "Iteration:  "; timeme(factorial_iteration, n, maxevals);
@@ -58,11 +58,11 @@ int main(int argc, char *argv[]){
 }
 
 
-double factorial_iteration(short n){
+double factorial_iteration(int n){
   double result = 1;
   if (n < 2) return result;
 
-  short i;
+  int i;
   for (i = 2; i < n+1; i++){
     result *= i;
   }
@@ -70,12 +70,12 @@ double factorial_iteration(short n){
 }
 
 
-double *factorial_iteration_p(short *n){
+double *factorial_iteration_p(int *n){
   double start = 1;
   double *result = &start;
   if (*n < 2) return result;
 
-  short i;
+  int i;
   for (i = 2; i < *n+1; i++){
     (*result) *= i;
   }
@@ -83,7 +83,7 @@ double *factorial_iteration_p(short *n){
 }
 
 
-double factorial_recursion(short n){
+double factorial_recursion(int n){
   if (n == 0){
      return 1;
   }
@@ -91,7 +91,7 @@ double factorial_recursion(short n){
 }
 
 
-double accumulator(short n, double acc){
+double accumulator(int n, double acc){
   if (n == 0){
     return acc;
   } else {
@@ -100,18 +100,19 @@ double accumulator(short n, double acc){
 }
 
 
-double factorial_accumulate(short n){
+double factorial_accumulate(int n){
   double start = 1;
   return accumulator(n, start);
 }
 
 
-void factorial_precision(short *result){
-  short n = result[0];
+void factorial_precision(int *result){
+  int n = result[0];
   result[0] = 1;
   int i, j, prod, carry, size = 1;
 
   for (i = 2; i <= n; i++){
+    carry = 0;
     for (j = 0; j < size; j++){
       prod = result[j] * i + carry;
       result[j] = prod % 10;
